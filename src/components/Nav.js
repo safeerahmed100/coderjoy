a5d03ab6c83f995c9eb87b0c6d9d69148e2cb4ac
Safeer Ahmed <SafeerAhmed@gmail.com>
2023-08-03 14:46:52 -0700

    Some minor changes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,19 +4,20 @@ import { Link,NavLink } from 'react-router-dom';
 function Nav() {
   
   return (
-   <nav className='flex items-center	justify-around pt-6 md
+   <nav className='flex items-center justify-around pt-6 md 
    '>
     <Link to="/">
     <h1>LOGO</h1>
     </Link>
-    <div className='navlinks flex space-x-4 '>
+    <div className='navlinks items-center flex space-x-4 '>
     <NavLink to="#Header">Home</NavLink>
 <NavLink to="#our-work" >Our Work</NavLink>
 <NavLink to="#services" >Services</NavLink>
 <NavLink to="#Packages" >Packages</NavLink>
 <NavLink to="#testimonial" >Testimonial</NavLink>
 <NavLink to="#Sign In" >Sign In</NavLink>
-<NavLink to="#Sign Up" ><button className='cursor-pointer hover:bg-sky-700'>Sign Up</button></NavLink>
+<div className='buttonarea'>
+<button className='cursor-pointer hover:bg-sky-700'>Sign Up</button> </div>
 </div>
    </nav>
   )
