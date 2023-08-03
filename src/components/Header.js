ad90ea1ca18b712b7fd433301b5519e9ccebc815
Safeer Ahmed <SafeerAhmed@gmail.com>
2023-08-03 16:03:56 -0700

    added basic transitions to side images

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,24 +11,26 @@ import './Header.css'
 
 
 function Header() {
-  const [changeImg,setChangeImg]=useState(leftImg);
   const [activeCircle,setActiveCircle]=useState(1);
+  const [switchImage,setSwitchImage]=useState(false);
+
+
+  const handleButtonClick = () => {
+    console.log("working hai jan")
+    setSwitchImage(prevState => !prevState);
+  };
 
   function handlePagination(index){
     setActiveCircle(index)
   }
-
-  function handleImg(){
-    setChangeImg(prevImg => (prevImg === leftImg ? rightImg : leftImg));  }
-  
     const centerImages= [Img1, Img2];
   return (
     <div className='Header w-screen bg-center	bg-cover	'>
       <div className='HeaderBg'></div>
               <Nav />
         <div className='hero flex items-center space-x-2  '>
-        <img  className="	 leftImg side" src={`${changeImg}`} alt='left' /> 
-        <i onClick={handleImg} className=' cursor-pointer bi bi-arrow-up-circle-fill animate-bounce toggle absolute bottom-20 left-36  '></i>   
+        <img  className={`leftImg side ${switchImage ? 'switchSideRight' : ''}`} src={leftImg} alt='left' /> 
+        <i  onClick={handleButtonClick} className=' cursor-pointer bi bi-arrow-up-circle-fill animate-bounce toggle absolute bottom-20 left-36  '></i>   
         <img className='sLeftImg cen' src={centerImages[activeCircle -1]} alt='sLeft' />
         <div  className='centerImg cen'> 
         <div className='blank'></div>
@@ -39,8 +41,8 @@ function Header() {
          <div className='blank'></div>
         </div>  
         <img  className='sRightImg cen' src={centerImages[activeCircle-2]} alt='right'/>   
-        <i onClick={handleImg} className='cursor-pointer bi bi-arrow-down-circle-fill  animate-bounce toggle absolute top-20 right-40  '></i>    
-        <img  className=" transition-opacity	 rightImg side"  src={`${changeImg=== rightImg? leftImg : rightImg}`} alt='right'/>    
+        <i onClick={handleButtonClick} className='cursor-pointer bi bi-arrow-down-circle-fill  animate-bounce toggle absolute top-20 right-40  '></i>    
+        <img  className={`rightImg side ${switchImage ? 'switchSideLeft' : ''}`} src={rightImg} alt='right'/>    
 
         </div>
         <div className='paginations'>
