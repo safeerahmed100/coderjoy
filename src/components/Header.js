480674e8ade814cc55100fc331afd06dfc75447f
Safeer Ahmed <SafeerAhmed@gmail.com>
2023-08-03 14:47:42 -0700

    some changes in slider

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from './Nav'
 import leftImg from '../assets/left.jpg';
 import rightImg from '../assets/right.jpg';
-import sLeftImg from '../assets/sLeft.jpg';
-import srightImg from '../assets/sright.jpg';
+import Img1 from '../assets/sLeft.jpg';
+import Img2 from '../assets/sright.jpg';
 import './Header.css'
 
 
@@ -11,14 +11,25 @@ import './Header.css'
 
 
 function Header() {
+  const [changeImg,setChangeImg]=useState(leftImg);
+  const [activeCircle,setActiveCircle]=useState(1);
+
+  function handlePagination(index){
+    setActiveCircle(index)
+  }
+
+  function handleImg(){
+    setChangeImg(prevImg => (prevImg === leftImg ? rightImg : leftImg));  }
+  
+    const centerImages= [Img1, Img2];
   return (
     <div className='Header w-screen bg-center	bg-cover	'>
       <div className='HeaderBg'></div>
               <Nav />
-        <div className='hero flex items-center space-x-2 
-'>
-        <img  className='leftImg side' src={leftImg} alt='left'/>    
-        <img className='sLeftImg cen' src={sLeftImg} alt='sLeft' />
+        <div className='hero flex items-center space-x-2  '>
+        <img  className="	 leftImg side" src={`${changeImg}`} alt='left' /> 
+        <i onClick={handleImg} className=' cursor-pointer bi bi-arrow-up-circle-fill animate-bounce toggle absolute bottom-20 left-36  '></i>   
+        <img className='sLeftImg cen' src={centerImages[activeCircle -1]} alt='sLeft' />
         <div  className='centerImg cen'> 
         <div className='blank'></div>
         <h1>Coder</h1>
@@ -27,12 +38,21 @@ function Header() {
         <button>Subscribe</button>
          <div className='blank'></div>
         </div>  
-        <img  className='sRightImg cen' src={srightImg} alt='right'/>    
-        <img  className='rightImg side' src={rightImg} alt='right'/>    
+        <img  className='sRightImg cen' src={centerImages[activeCircle-2]} alt='right'/>   
+        <i onClick={handleImg} className='cursor-pointer bi bi-arrow-down-circle-fill  animate-bounce toggle absolute top-20 right-40  '></i>    
+        <img  className=" transition-opacity	 rightImg side"  src={`${changeImg=== rightImg? leftImg : rightImg}`} alt='right'/>    
+
+        </div>
+        <div className='paginations'>
+        <span onClick={()=>handlePagination(1)} className={`circle  ${ activeCircle===1? 'activePag' :''}`}></span>
+          <span onClick={()=>handlePagination(2)} className={`circle  ${ activeCircle===2? 'activePag' :''}`}></span>
+         <span onClick={()=>handlePagination(3)} className={`circle ${ activeCircle===3? 'activePag' :''}`}></span>
 
         </div>
        </div>
   )
+  
+
 }
 
 export default Header
